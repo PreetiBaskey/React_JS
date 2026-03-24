b22e6cc8c15b6492b19384f0c962c63030ae174d
PreetiBaskey <38905783+PreetiBaskey@users.noreply.github.com>
2026-03-24 15:48:25 +0530

    Update React_JS_Note.jsx

--- React_JS_Note.jsx
+++ React_JS_Note.jsx
@@ -2,7 +2,7 @@
   Types of State Management
   1. Local State - useState
   2. Derived State
-  3. Global State
-  4. Server State
-  5. URL State
+  3. Global State - useContext(), Redux
+  4. Server State - useEffect(), React Query
+  5. URL State - React Router
 */
